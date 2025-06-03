1d60e45b8a7d897306aec3e139713145c5aa478c
FabioTeichmann <fabio.teichmann@stud.thga.de>
2025-06-03 18:17:25 +0200

    Add C printing solution (print.c)

---
+++ solutions/print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    // Begrüßung
+    printf("Hello from C!\n");
+
+    // Ganzzahl, Gleitkommazahl, String
+    printf("Integer: %d, Float: %.2f, String: %s\n", 42, 3.14, "test");
+
+    // Ausgabe mit puts (setzt automatisch einen Zeilenumbruch)
+    puts("This is puts output");
+
+    return 0;
+}
